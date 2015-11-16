754a54d098319ce129f06a77a38c7dd9bbba0877
rtung1333 <gnuttrebor07@gmail.com>
2015-11-16 17:08:45 -0500

    isolated processStage

--- process.c
+++ process.c
@@ -23,8 +23,7 @@
 //TODO: Free all allocated storage
 //
 
-
-int processHelper(CMD *cmd){
+int processStage(CMD *cmd){
 	int to;
 	int from;
 	if((cmd->toType == RED_OUT) || (cmd->toType == RED_OUT_APP)){
@@ -41,6 +40,84 @@ int processHelper(CMD *cmd){
 	pid_t pid;
 	int status = 0;
 	int secondFlag = 1;
+	if(cmd->left != 0){
+		leftStatus = processHelper(cmd->left);
+	}
+	switch(cmd->type){
+		case SIMPLE:
+			pid = fork();
+			if(pid == 0){
+				if(strcmp(cmd->argv[0],"cd") == 0){
+					if(cmd->argc < 2){
+						chdir(getenv("HOME"));
+					} else{
+						chdir(cmd->argv[1]);
+					}
+				} else if(strcmp(cmd->argv[0],"dirs") == 0){
+					char *buffer = malloc(sizeof(char)*(PATH_MAX + 1));
+					printf("%s\n",getcwd(buffer,PATH_MAX + 1));
+					free(buffer);
+				} else{
+					//child
+					if(dup2(to,STDO) == -1){
+						perror("dup2");
+						return errno;
+					}
+					if(dup2(from,STDI) == -1){
+						perror("dup2");
+						return errno;
+					}
+					for(int i=0;i<cmd->nLocal;i++){
+						if(setenv(cmd->locVar[i],cmd->locVal[i],1) != 0){
+							perror("setenv");
+							return errno;
+						}
+					}
+					status = execvp(cmd->argv[0],cmd->argv);
+					if(status < 0){
+						perror("execvp");
+						return errno;
+					}
+					for(int i=0;i<cmd->nLocal;i++){
+						if(unsetenv(cmd->locVar[i]) != 0){
+							perror("setenv");
+							return errno;
+						}
+					}
+				}
+			} else if(pid < 0){
+				perror("fork");
+				return errno;
+			} else{
+				//parent
+				if(leftStatus != 1){
+					waitpid(pid, &status, 0);
+				}
+			}
+			break;
+		case SUBCMD:
+			printf("PIPE\n");
+			break;
+		default:
+			fprintf(stderr,"Entered wrong function!\n");
+			break;
+	}
+	if((cmd->right != 0) && secondFlag){
+		processHelper(cmd->right);
+	}
+	if(to != STDO){
+		close(to);
+	}
+	if(from != STDI){
+		close(from);
+	}
+	return status;
+}
+
+int processHelper(CMD *cmd){
+	int leftStatus = 0;
+	int status = 0;
+	int secondFlag = 1;
 	if(cmd->type != PIPE){
 		if(cmd->left != 0){
 			leftStatus = processHelper(cmd->left);
@@ -48,55 +125,7 @@ int processHelper(CMD *cmd){
 
 		switch(cmd->type){
 			case SIMPLE:
-				pid = fork();
-				if(pid == 0){
-					if(strcmp(cmd->argv[0],"cd") == 0){
-						if(cmd->argc < 2){
-							chdir(getenv("HOME"));
-						} else{
-							chdir(cmd->argv[1]);
-						}
-					} else if(strcmp(cmd->argv[0],"dirs") == 0){
-						char *buffer = malloc(sizeof(char)*(PATH_MAX + 1));
-						printf("%s\n",getcwd(buffer,PATH_MAX + 1));
-						free(buffer);
-					} else{
-						//child
-						if(dup2(to,STDO) == -1){
-							perror("dup2");
-							return errno;
-						}
-						if(dup2(from,STDI) == -1){
-							perror("dup2");
-							return errno;
-						}
-						for(int i=0;i<cmd->nLocal;i++){
-							if(setenv(cmd->locVar[i],cmd->locVal[i],1) != 0){
-								perror("setenv");
-								return errno;
-							}
-						}
-						status = execvp(cmd->argv[0],cmd->argv);
-						if(status < 0){
-							perror("execvp");
-							return errno;
-						}
-						for(int i=0;i<cmd->nLocal;i++){
-							if(unsetenv(cmd->locVar[i]) != 0){
-								perror("setenv");
-								return errno;
-							}
-						}
-					}
-				} else if(pid < 0){
-					perror("fork");
-					return errno;
-				} else{
-					//parent
-					if(leftStatus != 1){
-						waitpid(pid, &status, 0);
-					}
-				}
+				status = processStage(cmd);
 				break;
 			case PIPE:
 				printf("PIPE\n");
@@ -120,7 +149,7 @@ int processHelper(CMD *cmd){
 				//fprintf(stderr,"Backgrounded: %d\n",pid);
 				break;
 			case SUBCMD:
-				secondFlag = 0;
+				status = processStage(cmd);
 				break;
 			case NONE:
 				printf("NONE\n");
@@ -136,18 +165,31 @@ int processHelper(CMD *cmd){
 		int *filedes = malloc(sizeof(int)*2);
 		if(pipe(filedes)==0){
 			//do pipe stuff
+			if(cmd->left != 0){
+				switch(cmd->left->type){
+					case SIMPLE:
+						//TODO
+						break;
+					case SUBCMD:
+						//TODO
+						break;
+					case PIPE:
+						//TODO
+						break;
+					default:
+						perror("pipe");
+						return errno;
+						break;
+				}
+			} else{
+				perror("Tree format");
+				return errno;
+			}
 		} else{
 			perror("pipe");
 			return errno;
 		}
 		free(filedes);
-	}
-
-	if(to != STDO){
-		close(to);
-	}
-	if(from != STDI){
-		close(from);
 	}
 	return status;
 }
