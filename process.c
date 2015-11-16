a080bc8de15681f7a8176f76ae5e9436aec83800
rtung1333 <gnuttrebor07@gmail.com>
2015-11-16 17:52:53 -0500

    started implementing pipe and extracting different process functions

--- process.c
+++ process.c
@@ -11,23 +11,104 @@
 #include <errno.h>
 #include "/c/cs323/Hwk5/process-stub.h"
 #include <sys/types.h>
+#include <assert.h>
 
 #define BG (1)
 #define STDI (0)
 #define STDO (1)
 #define EXIT_STATUS(x) (WIFEXITED(x) ? WEXITSTATUS(x) : 128+WTERMSIG(x))
 
+CMD **extractPipeChain(CMD *cmd, int *size, int *count);
+
+int processStage(CMD *cmd);
+
+int processHelper(CMD *cmd);
+
+int process(CMD *cmd);
+
 //TODO: implement signals
 //TODO: fix status updates
 //TODO: reap zombies periodically
 //TODO: Free all allocated storage
-//
 
+CMD **extractPipeChain(CMD *cmd, int *size, int *count){
+	//extract the array of pointers to cmd's that need to be piped together
+	CMD **pipeChain;
+	if(cmd->type == PIPE){
+		pipeChain = extractPipeChain(cmd->left,size,count);
+	} else if((cmd->type == SIMPLE) || (cmd->type == SUBCMD)){
+		pipeChain = malloc(sizeof(CMD *)*(*size));
+		pipeChain[0] = cmd->left;
+		assert((*count) == 0);
+		(*count)++;
+	} else{
+		perror("pipe");
+		return 0;
+	}
+	while((*size) <= (*count)){
+		(*size) *= 2;
+		pipeChain = realloc(pipeChain,sizeof(CMD *)*(*size));
+	}
+	pipeChain[(*count)] = cmd->right;
+	(*count)++;
+	return pipeChain;
+}
+
+int processPipe(CMD *cmd){
+	if(cmd->type != PIPE){
+		fprintf(stderr,"Entered wrong function!\n");
+		return errno;
+	}
+	int chainSize = 0;
+	int numPiped = 0;
+	CMD **pipeChain = extractPipeChain(cmd,&chainSize,&numPiped);
+	pipeChain = realloc(pipeChain,sizeof(CMD *)*(numPiped));
+
+	//TODO: use pipe.c on pipeChain
+
+	int *filedes = malloc(sizeof(int)*2);
+	if(pipe(filedes)==0){
+		//do pipe stuff
+		if(cmd->left != 0){
+			switch(cmd->left->type){
+				case SIMPLE:
+					//TODO
+					break;
+				case SUBCMD:
+					//TODO
+					break;
+				case PIPE:
+					//TODO
+					break;
+				default:
+					perror("pipe");
+					free(filedes);
+					free(pipeChain);
+					return errno;
+					break;
+			}
+		} else{
+			perror("Tree format");
+			free(filedes);
+			free(pipeChain);
+			return errno;
+		}
+	} else{
+		perror("pipe");
+		free(filedes);
+		free(pipeChain);
+		return errno;
+	}
+	free(filedes);
+	free(pipeChain);
+	//TODO: return value for this
+	return 1;
+}
 int processStage(CMD *cmd){
 	int to;
 	int from;
 	if((cmd->toType == RED_OUT) || (cmd->toType == RED_OUT_APP)){
-		to = open(cmd->toFile,O_WRONLY);
+		to = open(cmd->toFile,O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
 	} else{
 		to = STDO;
 	}
@@ -45,19 +126,19 @@ int processStage(CMD *cmd){
 	}
 	switch(cmd->type){
 		case SIMPLE:
-			pid = fork();
-			if(pid == 0){
-				if(strcmp(cmd->argv[0],"cd") == 0){
-					if(cmd->argc < 2){
-						chdir(getenv("HOME"));
-					} else{
-						chdir(cmd->argv[1]);
-					}
-				} else if(strcmp(cmd->argv[0],"dirs") == 0){
-					char *buffer = malloc(sizeof(char)*(PATH_MAX + 1));
-					printf("%s\n",getcwd(buffer,PATH_MAX + 1));
-					free(buffer);
+			if(strcmp(cmd->argv[0],"cd") == 0){
+				if(cmd->argc < 2){
+					chdir(getenv("HOME"));
 				} else{
+					chdir(cmd->argv[1]);
+				}
+			} else if(strcmp(cmd->argv[0],"dirs") == 0){
+				char *buffer = malloc(sizeof(char)*(PATH_MAX + 1));
+				printf("%s\n",getcwd(buffer,PATH_MAX + 1));
+				free(buffer);
+			} else{
+				pid = fork();
+				if(pid == 0){
 					//child
 					if(dup2(to,STDO) == -1){
 						perror("dup2");
@@ -84,14 +165,14 @@ int processStage(CMD *cmd){
 							return errno;
 						}
 					}
-				}
-			} else if(pid < 0){
-				perror("fork");
-				return errno;
-			} else{
-				//parent
-				if(leftStatus != 1){
-					waitpid(pid, &status, 0);
+				} else if(pid < 0){
+					perror("fork");
+					return errno;
+				} else{
+					//parent
+					if(leftStatus != 1){
+						waitpid(pid, &status, 0);
+					}
 				}
 			}
 			break;
@@ -162,34 +243,7 @@ int processHelper(CMD *cmd){
 		}
 	} else{
 		//pipe
-		int *filedes = malloc(sizeof(int)*2);
-		if(pipe(filedes)==0){
-			//do pipe stuff
-			if(cmd->left != 0){
-				switch(cmd->left->type){
-					case SIMPLE:
-						//TODO
-						break;
-					case SUBCMD:
-						//TODO
-						break;
-					case PIPE:
-						//TODO
-						break;
-					default:
-						perror("pipe");
-						return errno;
-						break;
-				}
-			} else{
-				perror("Tree format");
-				return errno;
-			}
-		} else{
-			perror("pipe");
-			return errno;
-		}
-		free(filedes);
+		processPipe(cmd);
 	}
 	return status;
 }
